67cb59c12b7a4e6cf226edc9e34da922983640b1
mack-1999 <makarandnikam1511@gmail.com>
2025-10-04 19:00:20 +0530

    Updated Example

--- Java Programes/Exception Handling/Mutliple_Catch.java
+++ Java Programes/Exception Handling/Mutliple_Catch.java
@@ -1,20 +1,16 @@
-class Mutliple_Catch{
+class Mutliple_Catch {
     public static void main(String[] args) {
-       try {
-           int arr[] = new int[5];
-
-           arr[5] = 30/0;
-           System.out.println(arr[5]);
-       } 
-       catch (ArithmeticException e) {
-           System.out.println("Arithmetic Exception");
-       } 
-       catch (ArrayIndexOutOfBoundsException e){
-           System.out.println("Array index out of bound");
-       }
-       catch (Exception e){
-           System.out.println(e);
-       }
-       System.out.println("Program End");
+        try {
+            int value = 30 / 0; // ArithmeticException
+            System.out.println(value);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bound");
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Program End");
+        }
     }
 }
