a122b9db60633f7c02293684a5a1605e9de4d94f
mack-1999 <makarandnikam1511@gmail.com>
2025-10-17 20:25:25 +0530

    Added new interview questions

--- Java Programes/Stream Api/Interview Questions/InterviewQue.java
+++ Java Programes/Stream Api/Interview Questions/InterviewQue.java
@@ -5,15 +5,23 @@ public class InterviewQue {
     public static void main(String[] args) {
         
         List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
-
-        // Q1. How to find even numbers in a list?
-        List<Integer> evenNumbers = numbers.stream().filter(num -> num % 2 ==0 ).toList();
-        System.out.println("Even Numbers: "+evenNumbers);
-
-        // Q2. How to find duplicate elements in a list?
         List<Integer> intNumbers = List.of(7,1,2,3,4,5,6,7,8,9,10,1,2,3,4);
+
+        //todo: Q1. How to find even numbers in a list?
+        List<Integer> evenNumbers = numbers.stream().filter(num -> num % 2 ==0 ).toList();
+        //System.out.println("Even Numbers: "+evenNumbers);
+
+        //todo: Q2. How to find duplicate elements in a list?
         List<Integer> duplicaIntegers = numbers.stream().filter(i -> intNumbers.indexOf(i) != intNumbers.lastIndexOf(i)).toList();
-        System.out.println("Duplicate Integers: "+duplicaIntegers);
+        //System.out.println("Duplicate Integers: "+duplicaIntegers);
+
+        //todo: Q3. How to find the second highest number in a list?
+        Integer secondHighest = intNumbers.stream().sorted((a,b) -> b.compareTo(a)).skip(1).findFirst().orElse(null);
+        //System.out.println("Second Highest: "+secondHighest);
+
+        //todo: Q4. Square of all numbers in a list?
+        List<Integer> intSquare = numbers.stream().map(num -> num * num).toList();
+        //System.out.println("Square of all numbers: "+intSquare);
 
     }
 }
