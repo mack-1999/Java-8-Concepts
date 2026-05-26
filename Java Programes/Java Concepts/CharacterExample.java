314d6c810cb67e1d980e6897697e024e5245a7b6
mack-1999 <makarandnikam1511@gmail.com>
2026-05-26 17:29:54 +0530

    Add CharacterExample and enhance WrapperClassExample with nullable int and parsing example

---
+++ Java Programes/Java Concepts/CharacterExample.java
@@ -0,0 +1,14 @@
+public class CharacterExample {
+        public static void main(String args[]) {
+
+        char ch = 'a';
+
+        if(ch > 70) {
+            System.out.println("Coding Ninjas");
+        }
+
+        else {
+            System.out.println("Ninjas");
+        }
+    }
+}
--- Java Programes/Java Concepts/WrapperClassExample.java
+++ Java Programes/Java Concepts/WrapperClassExample.java
@@ -11,12 +11,15 @@ public class WrapperClassExample {
         Double wrapperDouble = Double.valueOf(primitiveDouble);
         Character wrapperChar = Character.valueOf(primitiveChar);
         Boolean wrapperBoolean = Boolean.valueOf(primitiveBoolean);
+        Integer nullableInt = null; // Wrapper classes can be null, unlike primitives
 
         // Autoboxing (converting primitive to wrapper)
         Float wrapperFloat = (float) primitiveDouble; // Automatically converts primitive double to Float object
 
         // Unboxing (converting wrapper to primitive)
         int unboxedInt = wrapperInt; // Automatically converts Integer object to primitive int
+
+        int parsedInt = Integer.parseInt("123"); // Converts string to integer
 
         // Printing values
         System.out.println("Wrapper Integer: " + wrapperInt);
@@ -25,12 +28,13 @@ public class WrapperClassExample {
         System.out.println("Wrapper Character: " + wrapperChar);
         System.out.println("Wrapper Boolean: " + wrapperBoolean);
         System.out.println("Wrapper Float: " + wrapperFloat);
+        System.out.println("Parsed Integer: " + parsedInt);
     }
 }
 
 /*
  Wrapper classes:
-    In Java provide a way to use primitive data types (int, char, boolean, etc.) as objects.
+    Java provide a way to use primitive data types (int, char, boolean, etc.) as objects.
 
 Advantages of using wrapper classes:
      1. Wrapper classes provide useful methods for converting between types and mainpulating data.
