7fb2f64e47d73c0ba5285fc32e415a5d7304b9f0
mack-1999 <makarandnikam1511@gmail.com>
2026-05-28 09:29:02 +0530

    Refactor ArrayList example to improve clarity and add comments for better understanding

--- Java Programes/Collection Framework/Collection Interface/List Interface/ArrayListClass.java
+++ Java Programes/Collection Framework/Collection Interface/List Interface/ArrayListClass.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class ArrayListClass {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList();
-        list.add(10);
-        list.add(20);
-        // list.add("Mack"); // we can add different types of data
-        list.set(1, 50); // set() method is used to replace the element at specified index
+        list.add(0, 10);
+        list.add(1, 20);
+        list.add(2, 30);
+        list.set(2, 50); //! set() method is used to replace the element at specified index
 
-        System.out.println("List Size: "+list.size());
+        System.out.println("List Size: "+list.size()); //! size() method is used to get the number of elements in the list
 
         for(int elements: list){
             System.out.println("1. Arraylist Elements: "+elements);
         }
 
         List al = new ArrayList(); // Object type ArrayList
-        al.add(0, "Hello"); //Store data based on index
-        al.add(10);
-        al.add(true);
+        al.add(0, "Hello"); //! Store & retrieve data based on index
+        al.add(10); //! we can add different types of data
+        al.add(true); //? Every element get stored at last index iif we dont specify the index
 
         //forEach loop
         for (Object elements: al) {
@@ -43,6 +43,7 @@ public class ArrayListClass {
     2. Allows Duplicates
     3. Index based access
     4. Allows null values
+    
 
  > Initianal capacity of ArrayList is 10.
  > ArrayList internally uses Dynamic Array to store the elements.
@@ -53,4 +54,8 @@ public class ArrayListClass {
     2. Search: O(n)
     3. Insertion: O(n)
     4. Deletion: O(n)
+
+> When to use ArrayList over LinkedList:
+    1. When you need fast random access to elements 0(1) time complexity for get() method.
+    2. When you have a small number of insertions & deletions.
  */
