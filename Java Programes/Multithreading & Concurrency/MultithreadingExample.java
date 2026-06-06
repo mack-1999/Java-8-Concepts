48159c80cbc6562b39a11c34f7b381311bd71846
mack-1999 <makarandnikam1511@gmail.com>
2026-06-06 10:46:33 +0530

    Added VolatileKeyword example demonstrating shared resource management in multithreading

--- Java Programes/Multithreading & Concurrency/MultithreadingExample.java
+++ Java Programes/Multithreading & Concurrency/MultithreadingExample.java
@@ -1,5 +1,18 @@
 public class MultithreadingExample {
     public static void main(String[] args) {
         System.out.println("Running thread name: " + Thread.currentThread().getName());
+
+        Thread thread0 = new Thread(() -> {
+            System.out.println("Running thread name: " + Thread.currentThread().getName());
+        });
+        thread0.start();
+
+        Runnable runnable = () -> {
+            System.out.println("Running thread name: " + Thread.currentThread().getName());
+        };
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
+
+        
     }
 }
---
+++ Java Programes/Multithreading & Concurrency/VolatileKeyword.java
@@ -0,0 +1,35 @@
+class SharedResource {
+    private volatile boolean flag = false;
+
+    public void setFlag(boolean value) {
+        flag = value;
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+}
+
+public class VolatileKeyword {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000); // Simulate some work
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            sharedResource.setFlag(true);
+            System.out.println("Writer thread set flag to true");
+        }).start();
+
+        new Thread(() -> {
+            while (!sharedResource.getFlag()) {
+                // Busy-wait until flag is set to true
+            }
+            System.out.println("Reader thread detected flag is true");
+        }).start();
+
+    }
+}
