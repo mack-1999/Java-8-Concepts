84b767733c3fb807009836d25042854a6c1e6095
mack-1999 <makarandnikam1511@gmail.com>
2026-06-09 12:38:42 +0530

    MultithreadingExample for improved readability

--- Java Programes/Multithreading & Concurrency/MultithreadingExample.java
+++ Java Programes/Multithreading & Concurrency/MultithreadingExample.java
@@ -2,10 +2,9 @@ public class MultithreadingExample {
     public static void main(String[] args) {
         System.out.println("Running thread name: " + Thread.currentThread().getName());
 
-        Thread thread0 = new Thread(() -> {
+        new Thread(() -> {
             System.out.println("Running thread name: " + Thread.currentThread().getName());
-        });
-        thread0.start();
+        }).start();
 
         Runnable runnable = () -> {
             System.out.println("Running thread name: " + Thread.currentThread().getName());
@@ -13,6 +12,16 @@ public class MultithreadingExample {
         Thread thread1 = new Thread(runnable);
         thread1.start();
 
-        
     }
 }
+
+/*
+> Thread Class vs Runnable Interface:
+
+- Thread Class: You can create a thread by extending the Thread class and overriding its run() method. 
+  This approach allows you to directly create a thread object and start it.
+
+- Runnable Interface: You can also create a thread by implementing the Runnable interface and providing an implementation for the run() method.
+  This approach allows you to separate the thread's task from the thread's execution, making it more flexible.
+  
+*/
